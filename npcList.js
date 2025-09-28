e2a8acef97fe2526c40803ea01bf3c0606b48ad4
kevin\柯柯 <qazxsw1998870711@gmai.com>
2025-09-29 00:51:54 +0800

    新增npc Bird路線

--- npcList.js
+++ npcList.js
@@ -3,36 +3,36 @@ let npcList = [
     "chance": 0.33,
     "name": "Harry",
     "route": "M100,1175 L1800,1175 L1800,2050 L100,2050 L100,1175",
-    "motionPath":"M0 0 L1650 0 L1650 875 L-50 875 L0 0",
+    "motionPath":"M-50 0 L1650 0 L1650 875 L-50 875 L-50 0",
     "ribbon":{x:50, y:1050}
   },
   {
     "chance": 0.33,
     "name": "Hermione",
     "route": "M100,1175 L1800,1175 L1800,2050 L100,2050 L100,1175",
-    "motionPath":"M0 0 L1650 0 L1650 875 L-50 875 L0 0",
+    "motionPath":"M-50 0 L1650 0 L1650 875 L-50 875 L-50 0",
     "ribbon":{x:50, y:1050}
   },
   {
     "chance": 0.33,
     "name": "Ron",
-    "route": "M100,1250 L1800,1250 L1800,2050 L100,2050 L100,1250",
-    "motionPath":"M0 0 L1650 0 L1650 800 L-50 800 L0 0",
-    "ribbon":{x:50, y:1125}
+    "route": "M100,1175 L1800,1175 L1800,2050 L100,2050 L100,1175",
+    "motionPath":"M-50 0 L1650 0 L1650 875 L-50 875 L-50 0",
+    "ribbon":{x:50, y:1050}
   },
   {
     "chance": 0.01,
     "name": "Dobby",
     "route": "M100,1175 L1800,1175 L1800,2050 L100,2050 L100,1175",
-    "motionPath":"M0 0 L1650 0 L1650 875 L-50 875 L0 0",
+    "motionPath":"M-50 0 L1650 0 L1650 875 L-50 875 L-50 0",
     "ribbon":{x:50, y:1050}
   },
   {
     "chance": 0,
     "name": "Bird",
-    "route": "M100,2100 L100,500",
-    "motionPath":"M0 0 L0 -1600",
-    "ribbon":{x:0, y:1925}
+    "route": "M525,1175 L950,1175 L950,2050 L525,2050 L525,1175",
+    "motionPath":"M-50 0 L375 0 L375 875 L-50 875 L-50 0",
+    "ribbon":{x:475, y:1050}
   },
   {
     "chance": 0,
